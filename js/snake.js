2d12d97f3e2c35d0453892b2de9a45de85c13ab6
Abhibadan <abhibadanghoah1234@gmail.com>
2023-08-28 14:40:54 +0530

    mobile version added

--- js/snake.js
+++ js/snake.js
@@ -193,7 +193,7 @@
                 }
             }
         }
-        else if((desX-currentX>-100) && (desY-currentY<50)){
+        else if((desX-currentX<-100) && (desY-currentY<50)){
             if(setH==0){
                 rightmove=setInterval(() => {
                     l = snake_body[0].x;
@@ -220,7 +220,7 @@
                 }
             }
         }
-        else if((desX-currentX<20) && (desY-currentY<-100)){
+        else if((desX-currentX<50) && (desY-currentY<-100)){
             if(setV==0){
                 downmove=setInterval(() => {
                     l = snake_body[0].x;
